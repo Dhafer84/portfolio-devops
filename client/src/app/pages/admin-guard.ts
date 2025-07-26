04386f6191d88425a0afcb3ad85713482e1df435
Dhafer84 <boutheljad84@gmail.com>
2025-07-26 19:12:12 +0100

    Build Angular frontend for Render

--- client/src/app/pages/admin-guard.ts
+++ client/src/app/pages/admin-guard.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
-export class AdminGuard implements CanActivate {
+export class adminGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
