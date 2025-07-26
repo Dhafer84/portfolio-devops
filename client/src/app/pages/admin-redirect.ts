29fae3cbb099588f1bf48d08d2d93f35d71bf9e2
Dhafer84 <boutheljad84@gmail.com>
2025-07-27 00:24:44 +0100

    Build Angular frontend for Render

--- client/src/app/pages/admin-redirect.ts
+++ client/src/app/pages/admin-redirect.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
+  selector: 'app-admin-redirect',
   standalone: true,
-  template: ''
+  template: '',
 })
 export class AdminRedirectComponent implements OnInit {
   constructor(private router: Router) {}
