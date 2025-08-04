d3f01c9ffb902f55d040bf4cc0d0f97aa3b71daf
Dhafer84 <boutheljad84@gmail.com>
2025-08-04 11:28:07 +0100

    Build Angular frontend for Render

--- client/src/app/pages/skills/skills.ts
+++ client/src/app/pages/skills/skills.ts
@@ -27,4 +27,70 @@ export class SkillsComponent {
     { src: '/assets/Grafana.png', alt: 'Grafana' },
     { src: '/assets/Prometheus.png', alt: 'Prometheus' }
   ];
+  skillsFr = [
+    {
+      devops: 'Docker, Docker Compose',
+      qa: 'MISRA, LDRA, Coverity',
+      qualite: 'TRS, QRQC, 5S, 8D, 5M...'
+    },
+    {
+      devops: 'Kubernetes (K3s, AKS)',
+      qa: 'Revue de code, tests unitaires',
+      qualite: 'ISO 9001, IATF 16949, ISO26262, ASPICE'
+    },
+    {
+      devops: 'Jenkins, GitHub Actions, GitLab CI',
+      qa: 'SonarQube, Git',
+      qualite: 'Gestion Projets, SLA, SPC'
+    },
+    {
+      devops: 'Terraform, Ansible',
+      qa: 'Scripts Bash, YAML',
+      qualite: 'Technique de fabrication PCBA: CMS, Brasage...'
+    },
+    {
+      devops: 'Azure DevOps, Pipelines',
+      qa: 'Python (automatisation test)',
+      qualite: 'Formateur certifié CIT en IPC-A610'
+    },
+    {
+      devops: 'AWS, Azure (bases)',
+      qa: 'CI/CD systèmes embarqués',
+      qualite: 'Encadrement, audits internes'
+    }
+  ];
+
+  // === Compétences en EN ===
+  skillsEn = [
+    {
+      devops: 'Docker, Docker Compose',
+      qa: 'MISRA, LDRA, Coverity',
+      qualite: 'TRS, QRQC, 5S, 8D, 5M...'
+    },
+    {
+      devops: 'Kubernetes (K3s, AKS)',
+      qa: 'Code review, unit testing',
+      qualite: 'ISO 9001, IATF 16949, ISO26262, ASPICE'
+    },
+    {
+      devops: 'Jenkins, GitHub Actions, GitLab CI',
+      qa: 'SonarQube, Git',
+      qualite: 'Project Management, SLA, SPC'
+    },
+    {
+      devops: 'Terraform, Ansible',
+      qa: 'Bash & YAML scripting',
+      qualite: 'PCBA Manufacturing Techniques: SMT, Brazing...'
+    },
+    {
+      devops: 'Azure DevOps, Pipelines',
+      qa: 'Python (test automation)',
+      qualite: 'CIT-certified trainer in IPC-A610'
+    },
+    {
+      devops: 'AWS, Azure (basics)',
+      qa: 'Embedded CI/CD pipelines',
+      qualite: 'Coaching, internal audits'
+    }
+  ];
 }
